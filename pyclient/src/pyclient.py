1a09a37e76fbae9fc31af91324b34e7b2221ab4c
kofibreik <kofibreik@protonmail.com>
2020-12-12 12:37:01 -0500

    modify client script

--- pyclient/src/pyclient.py
+++ pyclient/src/pyclient.py
@@ -10,30 +10,30 @@ target_number = sys.argv[2]
 
 print ('connecting to %s port %s' % server_address, file=sys.stderr)
 
-sock.connect(server_address)
-sock_ip, sock_num = sock.getsockname()
-print("{} {} {}".format(sock_ip, sock_num, target_number))
+i=0
 
-while int(sock_num) <= int(target_number):
-    sock.close()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(server_address)
-    sock_ip, sock_num = sock.getsockname()
-    print("{} {}".format(sock_ip, sock_num))
+while int(i) <= int(target_number):
+  sock.close()
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect(server_address)
+  sock_ip, sock_num = sock.getsockname()
+  print("{} {}".format(sock_ip, sock_num))
 
-try:
+  try:
 
     message = 'This is the message.  It will be repeated.'
     print ('sending "%s"' % message,file=sys.stderr)
-    sock.sendall(message)
+    sock.sendall(message.encode())
 
     amount_received = 0
     amount_expected = len(message)
     while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print ('received "%s"' % data,file=sys.stderr)
+      data = sock.recv(16)
+      amount_received += len(data)
+      print ('received "%s"' % data,file=sys.stderr)
 
-finally:
-    sock.close()
+  finally:
+     sock.close()
+     print ('closing connection {} {}'.format(sock_ip, sock_num),file=sys.stderr)
 
+  i += 1
